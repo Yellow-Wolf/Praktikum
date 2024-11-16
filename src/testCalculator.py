e920148172a2499e33853d0df16eea36b2170e15
Kenntnisseller <86602689+Yellow-Wolf@users.noreply.github.com>
2024-11-16 21:39:18 +0300

    Update testCalculator.py

--- src/testCalculator.py
+++ src/testCalculator.py
@@ -2,7 +2,6 @@ import pytest
 from calculator import add, subtract, multiply, divide, power, factorial
 
 def test_add():
-    """Проверяет сумму."""
     assert add(1, 2) == 3
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
