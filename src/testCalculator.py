6d7ba0bab0421302f1155565ff8d2200ba7a60ff
Kenntnisseller <86602689+Yellow-Wolf@users.noreply.github.com>
2024-11-16 21:15:22 +0300

    Update testCalculator.py

--- src/testCalculator.py
+++ src/testCalculator.py
@@ -1,5 +1,5 @@
 import pytest
-from calculator import add, subtract, multiply, divide, power, factorial
+from src.calculator import add, subtract, multiply, divide, power, factorial
 
 def test_add():
     """Проверяет сумму."""
